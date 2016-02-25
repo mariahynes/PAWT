9358603b43642c7b9048f3e9e360f0858dffeb72
Maria <maria.m.hynes@gmail.com>
2016-02-25 21:07:57 +0000

    added jQuery in NAV

---
+++ javascript/jQuery.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+/*
+$(".container").on("click", function(){
+
+ 		
+		$(this).slideUp("fast");
+		
+
+	});
+*/
+
+
+
+$("#li_contact").on("click", function(){
+
+	$("#myNavbar li").removeClass("active");
+	$(this).addClass("active");
+
+});
+
+$("#li_tours").on("click", function(){
+
+	$("#myNavbar li").removeClass("active");
+	$(this).addClass("active");
+
+});
+
+$("#li_home").on("click", function(){
+
+	$("#myNavbar li").removeClass("active");
+	$(this).addClass("active");
+	
+});
+
+$("#li_sculpture").on("click", function(){
+
+	$("#myNavbar li").removeClass("active");
+	$(this).addClass("active");
+	
+});
+
+$("#li_reading").on("click", function(){
+
+	$("#myNavbar li").removeClass("active");
+	$(this).addClass("active");
+	
+});
+
+$("#li_art").on("click", function(){
+
+	$("#myNavbar li").removeClass("active");
+	$(this).addClass("active");
+	
+});
+
+$("#li_login").on("click", function(){
+
+	$("#myNavbar li").removeClass("active");
+	$("#li_home").addClass("active");
+
+});
+
+});
