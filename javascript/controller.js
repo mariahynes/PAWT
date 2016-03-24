bc82f44e6e13606e7793a93ac052b8e28d84e281
Maria <maria.m.hynes@gmail.com>
2016-03-24 15:03:34 +0000

    added contact form and updated css, no form processing yet

--- javascript/controller.js
+++ javascript/controller.js
@@ -9,8 +9,6 @@ angular.module("routingControllers").controller("AboutController", function($sco
 
 angular.module("routingControllers").controller("HomeController", function($scope){
 
-	var self = this;
-	self.message = "This is the Home Page";
 
 });
 
@@ -55,30 +53,79 @@ angular.module("routingControllers").controller("ToursController", function($sco
 
 });
 
-angular.module("routingControllers").controller("ContactController", function($scope){
+angular.module("routingControllers").controller("ContactController", function($scope,$http){
 
-	var self = this;
-	self.message = "This is the Home Page";
+	$scope.user = {};
+	$scope.submitted = false;
+	$scope.submitMessage = "";
+	$scope.errorEmail= "";
+	$scope.emailsMatch = false;
+	
+	$scope.feedbackForm = function(feedbackForm){
+	
+		if(feedbackForm.$valid){
+		
+			if($scope.user.email === $scope.user.email2){
+				$scope.submitted = true;
+				//send email service???
+				$scope.submitMessage = "Thank you for your message, " + $scope.user.firstname;
+				$scope.errorEmail = "";
+				$scope.emailsMatch = true;
+				
+				$http({
+					  method  : 'POST',
+					  url     : 'process.php',
+					  data    : $.param($scope.user),  // pass in data as strings
+					  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data 
+				});
+				
+				
+				 
+				
+			}
+			else{
+				$scope.errorEmail = "Sorry, your email addresses do not match.";
+				$scope.submitted = false;
+				$scope.emailsMatch = false;
+			}
+		}
+		else{
+			$scope.submitted = true;
+			$scope.errorEmail = "";
+			$scope.submitMessage = "Sorry, your message is not valid.";
+		}
+	};
+	
+	$scope.formReload = function(){
+		$scope.submitted = false;
+		$scope.user.message = "";
+		$scope.submitMessage = "";
+		$scope.errorEmail = "";
+	};
+	
+	$scope.clearForm = function(){
+		$scope.submitted = false;
+		$scope.errorEmail = "";
+		$scope.submitMessage = "";
+		$scope.user = {};
+	};
 
 });
 
 angular.module("routingControllers").controller("SculptureController", function($scope){
 
-	var self = this;
-	self.message = "This is the Home Page";
+
 
 });
 
 angular.module("routingControllers").controller("ReadingController", function($scope){
 
-	var self = this;
-	self.message = "This is the Home Page";
+
 
 });
 
 angular.module("routingControllers").controller("ArtLinksController", function($scope){
 
-	var self = this;
-	self.message = "This is the Home Page";
+
 
 });
